0c67a701ffca68acb24d5010182f0df8506a0b4c
Djilaxx <djillali.hadmoussa@orange.fr>
2021-05-08 22:13:33 +0200

    DATASET & TRAINING FUNCTIONS

---
+++ datasets/nlp_dataset.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from utils.clean_text import pre_process_text
+
+class TEXT_DS(Dataset):
+    def __init__(self, model_name, text, labels, max_len, tokenizer = None):
+        self.model_name = model_name
+        self.text = text
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        
+    def __getitem__(self, index):
+        text = str(self.text[index])
+        text = pre_process_text(text)
+        inputs = self.tokenizer.encode_plus(
+            text,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            return_token_type_ids=True,
+            truncation=True
+        )
+
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'masks': torch.tensor(mask, dtype=torch.long),
+            'labels': torch.tensor(self.labels[index], dtype=torch.long)
+        }
+        
+    def __len__(self):
+        return len(self.text)
--- train.py
+++ train.py
@@ -18,6 +18,7 @@ from sklearn import model_selection
 # MY OWN MODULES
 from datasets import nlp_dataset
 from utils import early_stopping, folding
+from utils.metrics import metrics_dict
 from trainer.trainer import Trainer
 
 ##################
@@ -40,15 +41,73 @@ def train(folds=5, project="tweet_disaster", model_name="distilbert", model_type
     # LOADING DATA FILE & TOKENIZER
     df = pd.read_csv(config.main.FOLD_FILE)
 
-    # FEATURE ENGINEERING
-
-    # MODEL & TOKENIZER
+    # MODEL 
+    # NEED TO BE ADAPTED TO BE ABLE TO RUN CLASSIC MODELS AND TRANSFORMERS WITH LOADED CONFIG #####
+    for name, func in inspect.getmembers(importlib.import_module(f"models.{complete_name}"), inspect.isfunction):
+        if name == complete_name:
+            model = func(**config.model[complete_name])
 
     # METRIC
-
+    metric_selected = metrics_dict[config.train.METRIC]
     # FOLD LOOP
+    for fold in range(folds):
+        print(f"Starting training for fold : {fold}")
 
-    
+        # CREATING TRAINING AND VALIDATION SETS
+        df_train = df[df.kfold != fold].reset_index(drop=True)
+        df_valid = df[df.kfold == fold].reset_index(drop=True)
+
+        model.to(config.main.DEVICE)
+        ########################
+        # CREATING DATALOADERS #
+        ########################
+        # TRAINING IDs & LABELS
+        trn_text = df_train[config.main.TEXT_VAR].values.tolist()
+        trn_labels = df_train[config.main.TARGET_VAR].values
+        # VALIDATION IDs & LABELS
+        valid_text = df_valid[config.main.TEXT_VAR].values.tolist()
+        valid_labels = df_valid[config.main.TARGET_VAR].values
+        # TRAINING DATASET
+        train_ds = nlp_dataset.NLP_DATASET(
+            model_name = complete_name,
+            text = trn_text,
+            labels = trn_labels,
+            max_len = config.main.MAX_LEN,
+            tokenizer = config.train.tokenizer,
+        )
+        # TRAINING DATALOADER
+        train_loader = torch.utils.data.DataLoader(
+            trn_ds, 
+            batch_size=config.train.TRAIN_BS, 
+            shuffle=True, 
+            num_workers=0
+        )
+        # VALIDATION DATASET
+        valid_ds = text_ds.TEXT_DS(
+            model_name = complete_name,
+            text = valid_text,
+            labels = valid_labels,
+            max_len = config.main.MAX_LEN,
+            tokenizer = config.train.tokenizer
+        )
+        # VALIDATION DATALOADER
+        valid_loader = torch.utils.data.DataLoader(
+            valid_ds, 
+            batch_size=config.hyper.VALID_BS, 
+            shuffle=True, 
+            num_workers=0
+        )
+
+        # IMPORT LOSS FUNCTION
+        loss_module = importlib.import_module(f"loss.{loss}")
+        criterion = loss_module.loss_function()
+        # SET OPTIMIZER, SCHEDULER
+        optimizer = torch.optim.Adam(model.parameters(), lr=config.train.LR)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
+        # SET EARLY STOPPING FUNCTION
+        es = early_stopping.EarlyStopping(patience=2, mode="max")
+        # CREATE TRAINER
+        trainer = Trainer(model, optimizer, config.main.DEVICE, criterion)
 ##########
 # PARSER #
 ##########

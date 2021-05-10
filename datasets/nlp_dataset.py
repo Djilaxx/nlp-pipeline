c1e68063c5d57ed4f60c6fd0bff9b6af009889cd
Djilaxx <djillali.hadmoussa@orange.fr>
2021-05-10 17:42:32 +0200

    WORKING ON DATASET FUNCTION
    - Defining number of class function needed for different NLP task
    - Defining structure to be able to feed different kind of models (transformers, LSTM, xgboost etc...)

--- datasets/nlp_dataset.py
+++ datasets/nlp_dataset.py
@@ -2,26 +2,37 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from utils.clean_text import pre_process_text
 
-class TEXT_DS(Dataset):
-    def __init__(self, model_name, text, labels, max_len, tokenizer = None):
+class NLP_DATASET(Dataset):
+    def __init__(self, model_name, text, labels, max_len, tokenizer = None, feature_eng = None):
         self.model_name = model_name
         self.text = text
         self.labels = labels
-        self.tokenizer = tokenizer
         self.max_len = max_len
-        
+        self.tokenizer = tokenizer
+        self.feature_eng = feature_eng
+
+    #RETURN THE LENGHT OF THE DATASET
+    def __len__(self):
+        return len(self.text)
+    
+    #FUNCTION THAT RETURN ONE DATAPOINT (INPUT + LABEL)
     def __getitem__(self, index):
+        # LIST WHERE ONE ROW OF TEXT DATA
         text = str(self.text[index])
-        text = pre_process_text(text)
-        inputs = self.tokenizer.encode_plus(
-            text,
-            None,
-            add_special_tokens=True,
-            max_length=self.max_len,
-            pad_to_max_length=True,
-            return_token_type_ids=True,
-            truncation=True
-        )
+
+        if feature_eng is not None:
+            text = feature_eng(text)
+
+        if tokenizer is not None:
+            inputs = self.tokenizer.encode_plus(
+                text,
+                None,
+                add_special_tokens=True,
+                max_length=self.max_len,
+                pad_to_max_length=True,
+                return_token_type_ids=True,
+                truncation=True
+                )
 
         ids = inputs['input_ids']
         mask = inputs['attention_mask']
@@ -31,5 +42,44 @@ class TEXT_DS(Dataset):
             'labels': torch.tensor(self.labels[index], dtype=torch.long)
         }
         
+class NLP_QA_DATASET(Dataset):
+    def __init__(self, model_name, text, labels, max_len, tokenizer = None, feature_eng = None):
+        self.model_name = model_name
+        self.text = text
+        self.labels = labels
+        self.max_len = max_len
+        self.tokenizer = tokenizer
+        self.feature_eng = feature_eng
+    
+    #RETURN THE LENGHT OF THE DATASET
     def __len__(self):
         return len(self.text)
+    
+    #FUNCTION THAT RETURN ONE DATAPOINT (INPUT + LABEL)
+    def __getitem__(self, index):
+        
+        # LIST WHERE ONE ROW OF TEXT DATA
+        text = str(self.text[index])
+
+        if feature_eng is not None:
+            text = feature_eng(text)
+
+        
+        if tokenizer is not None:
+            inputs = self.tokenizer.encode_plus(
+                text,
+                None,
+                add_special_tokens=True,
+                max_length=self.max_len,
+                pad_to_max_length=True,
+                return_token_type_ids=True,
+                truncation=True
+                )
+
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'masks': torch.tensor(mask, dtype=torch.long),
+            'labels': torch.tensor(self.labels[index], dtype=torch.long)
+        }
